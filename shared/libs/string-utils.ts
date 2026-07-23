e4b6d17124f8d687e1504ac3d8435de78b314cac
ikaydreams-dev <ikaydreams108@gmail.com>
2026-07-23 01:24:58 +0000

    Add string and array utility libraries

---
+++ shared/libs/string-utils.ts
@@ -0,0 +1,56 @@
+export class StringUtils {
+  static capitalize(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+
+  static camelCase(str: string): string {
+    return str
+      .replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''))
+      .replace(/^(.)/, (c) => c.toLowerCase());
+  }
+
+  static snakeCase(str: string): string {
+    return str
+      .replace(/([A-Z])/g, '_$1')
+      .toLowerCase()
+      .replace(/^_/, '');
+  }
+
+  static kebabCase(str: string): string {
+    return str
+      .replace(/([a-z])([A-Z])/g, '$1-$2')
+      .replace(/[\s_]+/g, '-')
+      .toLowerCase();
+  }
+
+  static truncate(str: string, length: number, suffix: string = '...'): string {
+    if (str.length <= length) return str;
+    return str.substring(0, length - suffix.length) + suffix;
+  }
+
+  static slugify(str: string): string {
+    return str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '');
+  }
+
+  static randomString(length: number): string {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  }
+
+  static countWords(str: string): number {
+    return str.trim().split(/\s+/).length;
+  }
+
+  static reverse(str: string): string {
+    return str.split('').reverse().join('');
+  }
+}
