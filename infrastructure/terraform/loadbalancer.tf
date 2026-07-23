5f704a48dde78e94a0e7a34e6cb8292b8eeedf69
ikaydreams-dev <ikaydreams108@gmail.com>
2026-07-23 01:26:06 +0000

    Add Terraform load balancer configuration

---
+++ infrastructure/terraform/loadbalancer.tf
@@ -0,0 +1,46 @@
+resource "aws_lb" "main" {
+  name               = "booster-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.app.id]
+  subnets            = aws_subnet.public[*].id
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name        = "booster-lb"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "gateway" {
+  name     = "booster-gateway-tg"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/health"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.gateway.arn
+  }
+}
