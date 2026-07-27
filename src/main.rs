280591d49f3291dfc1c75dc53e43e32e3b9dbb4b
ikaydreams-dev <ikaydreams108@gmail.com>
2026-07-27 09:21:32 +0000

    Add Rust main entry point

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Booster Syndrome - Multi-language Microservices");
+    println!("Version: 1.0.0");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
